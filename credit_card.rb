4bfdf10917e933cea0f54e20da4c708882f7d219
Richard Beasley <richardbeasley27@gmail.com>
2015-10-16 08:20:33 -0600

    Update credit_card.rb

--- credit_card.rb
+++ credit_card.rb
@@ -1,11 +1,14 @@
 # Class Comment
 class CreditCard
-  attr_reader :balance
+  attr_reader :balance,:payment
   def initialize
     @balance = 0.00
   end
 
   def charge(amount)
     @balance += amount
+  end
+    def payment(amount)
+    @balance -= amount
   end
 end
